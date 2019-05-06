8e9f04d5951bdba1efa3213613b7554a75c796d7
houzhenhong <houzhenhong@hotmail.com>
2019-05-07 00:13:08 +0800

    chore(*): init

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: ['webpack/hot/poll?100', './src/main.ts'],
+  watch: true,
+  target: 'node',
+  externals: [
+    nodeExternals({
+      whitelist: ['webpack/hot/poll?100'],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@/src': path.resolve(`./src/`),
+    }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // 排除 js 和 d.ts
+    new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/]),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'server.js',
+  },
+};
