539a2668b28c8440d49200860072311b341f520c
houzhenhong <houzhenhong@hotmail.com>
2019-05-07 17:26:31 +0800

    fix(*): webpack与 typeorm 的问题
    https://github.com/nestjs/nest/issues/755#issuecomment-394073763

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+import { entityContext } from '@/src/entity.context';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    keepConnectionAlive: true,
+    type: "postgres",
+    host: "192.168.99.100",
+    port: 5432,
+    username: "postgres",
+    database: "module_user",
+    entities: [...entityContext.keys().map(id => {
+      const entityModule = entityContext(id);
+      const [entity] = Object.values(entityModule);
+      return entity;
+  })],
+    synchronize: true
+  }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
