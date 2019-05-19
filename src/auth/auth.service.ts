fad6ab001a0ebbaffaea2ad7cc762eff02b140d8
houzhenhong <houzhenhong@hotmail.com>
2019-05-19 13:10:23 +0800

    feat(env): 敏感信息使用 环境变量

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,19 +3,19 @@ import * as jwt from 'jsonwebtoken';
 
 import { JwtPayload, JwtReply } from '../interfaces/jwt.interface';
 
+const { SECRET_KEY } = process.env
 @Injectable()
 export class AuthService {
-
     // TODO: 配置有效期
     async createToken(payload: JwtPayload): Promise<JwtReply> {
         const expiresIn = Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 30)
-        const accessToken = jwt.sign({...payload, exp: expiresIn}, 'secretKey')
+        const accessToken = jwt.sign({...payload, exp: expiresIn}, SECRET_KEY)
         return { accessToken, expiresIn }
     }
 
     async createSuperAdminToken(payload: { userId: string }): Promise<JwtReply> {
         const expiresIn = Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 1200)
-        const accessToken = jwt.sign({...payload, role: 'admin', exp: expiresIn}, 'secretKey')
+        const accessToken = jwt.sign({...payload, role: 'admin', exp: expiresIn}, SECRET_KEY)
         return { accessToken, expiresIn }
     }
 }
